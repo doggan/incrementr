fcfd02b853a04a89e19413cc1c805050a4d76b96
Shyam Guthikonda <shyamguth@gmail.com>
2020-09-29 20:46:13 -0700

    Updates

---
+++ precache-manifest.7405c0bffa60133bbfdbedab75e3e2c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a9caec8b0af56b324a",
+    "url": "css/app.1d558ded.css"
+  },
+  {
+    "revision": "acecfad7daaf7887c2f1",
+    "url": "css/chunk-vendors.e9eba236.css"
+  },
+  {
+    "revision": "ac0f383127a63f98e3d0783114c497a1",
+    "url": "index.html"
+  },
+  {
+    "revision": "d4a9caec8b0af56b324a",
+    "url": "js/app.0e2a7dae.js"
+  },
+  {
+    "revision": "acecfad7daaf7887c2f1",
+    "url": "js/chunk-vendors.41328d2d.js"
+  },
+  {
+    "revision": "f9159e1ed7c8f510e8961608223674ae",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
